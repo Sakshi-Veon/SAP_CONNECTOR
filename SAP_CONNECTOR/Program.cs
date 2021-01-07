ac7d2ea9b1fccbd0c685d9efc234fef77d95a1cb
Sakshi Singh <sakshi.s@veonconsulting.com>
2021-01-07 17:19:21 +0530

    added comment
    Signed-off-by: Sakshi Singh <sakshi.s@veonconsulting.com>

--- SAP_CONNECTOR/Program.cs
+++ SAP_CONNECTOR/Program.cs
@@ -12,6 +12,7 @@ namespace SAP_CONNECTOR
         /// The main entry point for the application.
         /// SAP Connector
         /// Sravani SAP Connector
+        /// Sakshi SAP Connector
         /// </summary>
         [STAThread]
         static void Main()
